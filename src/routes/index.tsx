56e36d5881a1fa3035174af0c2b61e0ec6b7efb4
genekellyboyle <DrDuffy@bhhsnv.com>
2025-09-16 16:46:26 -0700

    Fix import organization for LuxuryHomeCards test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { SocialProof, LuxuryHomeCards } from "~/components/luxury";
+import { LuxuryHomeCards, SocialProof } from "~/components/luxury";
 
 export default component$(() => {
 	return (
