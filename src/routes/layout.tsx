819bd5d448f92b3c06d38e2a63be94f3d644667a
genekellyboyle <DrDuffy@bhhsnv.com>
2025-09-16 21:40:56 -0700

    Fix footer: Replace Builder.io starter footer with custom Dr. Jan Duffy footer
    - Updated layout.tsx to import custom Footer component instead of starter footer
    - Removes 'Made with ♡ by Builder.io' text from footer
    - Now displays proper Dr. Jan Duffy branding and contact information

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,6 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
-import Footer from "~/components/starter/footer/footer";
+import { Footer } from "~/components/footer";
 import Header from "~/components/starter/header/header";
 
 import styles from "./styles.css?inline";
