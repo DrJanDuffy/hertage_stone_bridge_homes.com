0005f95820b49b8cf24d1518b3e372eb0b5437c3
genekellyboyle <DrDuffy@bhhsnv.com>
2025-09-16 23:46:07 -0700

    🔧 Remove advanced components temporarily to fix deployment
    - Temporarily removed PerformanceMonitor, AdvancedCaching, and AdvancedSEO components
    - This should resolve the deployment loop and get the site working
    - Will re-add optimizations once basic deployment is stable

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,9 +2,6 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import { Footer } from "~/components/footer";
 import Header from "~/components/starter/header/header";
-import { PerformanceMonitor } from "~/components/performance/PerformanceMonitor";
-import { AdvancedCaching } from "~/components/optimization/AdvancedCaching";
-import { AdvancedSEO } from "~/components/seo/AdvancedSEO";
 
 import styles from "./styles.css?inline";
 
@@ -18,15 +15,6 @@ export default component$(() => {
 	useStyles$(styles);
 	return (
 		<>
-			{/* Advanced Performance Monitoring */}
-			<PerformanceMonitor />
-			
-			{/* Advanced Caching Strategy */}
-			<AdvancedCaching />
-			
-			{/* Advanced SEO Optimization */}
-			<AdvancedSEO />
-			
 			<Header />
 			<main>
 				<Slot />
